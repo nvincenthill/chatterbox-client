f94920c6415d6d176542c3fcfea87411ece88c6d
Hack Reactor Student <student@hackreactor.com>
2018-06-22 11:56:20 -0700

    Implement AJAX recurring GET request

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,39 @@
 // YOUR CODE HERE:
+// endpoint = http://parse.sfm8.hackreactor.com/chatterbox/classes/messages
+
+let endpoint = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages'
+
+// example AJAX request
+
+$(document).ready(function() {
+    console.log( "document loaded" );
+    setInterval(() => {
+      retrieveMessages()}, 1000);
+})
+
+$( window ).on( "load", function() {
+    console.log( "window loaded" );
+});
+
+let retrieveMessages = function() {
+  $.ajax({
+    url: endpoint,
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log(data);
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+
+let success = () => {
+  console.log('got data');
+}
+
+let fail = () => {
+  console.log('failed');
+}
